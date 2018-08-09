b0fb800bed32cfee157e30c866897a5e2089fe59
RyanSquared <vandor2012@gmail.com>
2018-08-09 16:42:32 -0500

    lambda_function: Add better error handling

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,4 @@
 import contextlib
-import datetime
 import json
 import uuid
 import os
@@ -31,12 +30,26 @@ bucket = boto3.resource('s3').Bucket(BUCKET_NAME)
 iot = boto3.client('iot-data')
 
 
+def error_from_exception(exc):
+    return {"statusCode": 400, "body": json.dumps({
+            "message": str(exc), "type": str(type(exc))})}
+
+
 def lambda_handler(event, context):
-    json_data = json.loads(event["body"])
+    try:
+        json_data = json.loads(event["body"])
+    except json.decoder.JSONDecodeError as e:
+        return error_from_exception(e)
     devices = []
 
     # Check whether `target` is Device or Group {{{
-    arn = json_data["target"]
+    try:
+        arn = json_data["target"]
+        text = json_data["message"]
+    except KeyError as e:
+        return error_from_exception(e)
+
+    is_group = False
     item = session.query(Device).filter_by(arn=arn).first()
     if item is not None:
         # Found a device
@@ -45,13 +58,14 @@ def lambda_handler(event, context):
         # Check if `target` is Group
         group = session.query(Group).filter_by(arn=arn).first()
         if group is not None:
+            is_group = True
             devices.extend(group.devices)
     # }}}
 
     # Generate mp3 file {{{
     response = polly.synthesize_speech(
         OutputFormat="mp3",
-        Text=json_data["message"],
+        Text=text,
         TextType="text",
         VoiceId=json_data.get("voice_id", DEFAULT_VOICE))
     output = str(uuid.uuid4()) + ".mp3"
@@ -71,17 +85,17 @@ def lambda_handler(event, context):
             if json_data.get("testing"):
                 will_publish = False
 
-    payload = json.dumps({
-        "message": json_data["message"],
+    payload = {
+        "message": text,
         "uri": s3.generate_presigned_url("get_object", Params={
             "Bucket": BUCKET_NAME,
             "Key": output
         }),
         "published": will_publish
-    })
+    }
     args = {
         "topic": "noticast-messages",
-        "payload": payload
+        "payload": json.dumps(payload)
     }
 
     if will_publish:
@@ -91,6 +105,8 @@ def lambda_handler(event, context):
             iot.publish(**args)
 
     # }}}
+
+    payload.update(devices=[d.arn for d in devices], is_group=is_group)
 
     return {
         "statusCode": 200,
